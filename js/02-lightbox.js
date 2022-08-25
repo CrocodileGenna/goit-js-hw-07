d0ae65d6b8bff989c9eba422da5cef91e2b4ed87
Moskalenko Yurii <yuramus1998@gmail.com>
2022-08-25 20:35:27 +0300

    comit 5

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,14 +2,13 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-
 const imgConteiner = document.querySelector(".gallery");
 
 const creatItem = (galleryItems) => {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-            <div class="gallery__item">
+            
                 <a class="gallery__link" href="${original}">
                 <img
                     class="gallery__image"
@@ -17,40 +16,15 @@ const creatItem = (galleryItems) => {
                     data-source="${original}"
                     alt="${description}"
                 />
-                </a>
-            </div>`;
+                </a>`;
     })
     .join("");
 };
 
 imgConteiner.insertAdjacentHTML("beforeend", creatItem(galleryItems));
 
-imgConteiner.addEventListener("click", openImg);
-
-const factotis = new SimpleLightbox(".galery a", {
-  caption: true,
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
   captionDelay: 250,
-  captionData: "alt",
+  captionsData: "alt",
 });
-
-function openImg(e) {
-  e.preventDefault();
-  const galleryImg = e.target.classList.contains("gallery__image");
-  if (!galleryImg) {
-    return;
-  }
-  const eventImg = e.target.dataset.source;
-
-  let instance = basicLightbox.create(`
-    <img src="${eventImg}"  width="800" height="600">
-`);
-
-  instance.show();
-  imgConteiner.addEventListener("keydown", escapeFunction);
-  function escapeFunction(e) {
-    if (e.code !== "Escape") {
-      return;
-    }
-    const elem = instance.close();
-  }
-}
